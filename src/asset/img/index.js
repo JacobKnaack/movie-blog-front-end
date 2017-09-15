d22a411a6d737ce391e655766f1f6fa44d4dcf65
Jacob Knaack <jacobknck@gmail.com>
2017-09-15 15:50:48 -0700

    added review

--- src/asset/img/index.js
+++ src/asset/img/index.js
@@ -3,13 +3,15 @@ import WelcomeGraphic from './welcome-graphic.svg'
 import AtomicBlondeImage from './atomic-blonde.jpg'
 import DunkirkImage from './dunkirk.jpg'
 import NakedImage from './naked.jpg'
+import ItImage from './it.jpg'
 
 const Images = {
   reviewImages: [
     ValerianImage,
     AtomicBlondeImage,
     DunkirkImage,
-    NakedImage
+    NakedImage,
+    ItImage
   ],
   HeaderGraphic: WelcomeGraphic,
 }
